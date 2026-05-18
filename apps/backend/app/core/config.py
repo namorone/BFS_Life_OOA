88e1ebec6e0b54fcdbc6bf171322df059f6d02d0
Oleksandr Zhenchenko <s.o.zhenchenkowww@gmail.com>
2026-05-18 16:47:26 +0300

    idk

--- apps/backend/app/core/config.py
+++ apps/backend/app/core/config.py
@@ -18,6 +18,8 @@ class Settings(BaseSettings):
         "http://127.0.0.1:3000",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:3001",
+		"http://127.0.0.1:3001",
     ]
     MEDIA_ROOT: str = "./media"
     WARRANTY_EXPIRING_DAYS: int = 30
